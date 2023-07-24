fd8f51a97173e25f3158101a9ec649773c48564c
Hammerian <hammerian@hotmail.com>
2023-07-24 19:47:37 +0200

    Join Branches.

---
+++ CursoJava/src/practicas_Java_230724/edadPersona.java
@@ -0,0 +1,33 @@
+package practicas_Java_230724;
+
+import java.util.Scanner;
+
+public class edadPersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int edad = 1;
+		while (edad>0) {
+			System.out.println("Introduce la edad de la persona:");
+			edad = scn.nextInt();
+
+			if (edad > 17) {
+				System.out.println("La persona es mayor de edad");
+				if (edad > 74) {
+					System.out.println("La persona es anciana");
+				} else {
+					System.out.println("La persona es adulta");
+				}
+			} else {
+				System.out.println("La persona es menor de edad");
+				if (edad > 11) {
+					System.out.println("La persona es adolescente");
+				} else {
+					System.out.println("La persona es infantil");
+				}
+			}
+		}
+	}
+
+}
---
+++ CursoJava/src/practicas_Java_230724/numeroDivisible.java
@@ -0,0 +1,34 @@
+package practicas_Java_230724;
+
+import java.util.Scanner;
+
+public class numeroDivisible {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Introduce un número:");
+		int numero = scn.nextInt();
+		
+		if (numero % 2==0) {
+			System.out.println("El número es divisible por 2");
+			if (numero % 3==0) {	
+				System.out.println("El número es divisible por 3");
+				if (numero % 5==0) {	
+					System.out.println("El número es divisible por 5");
+				}
+			}
+		} else if (numero % 3==0) {	
+			System.out.println("El número es divisible por 3");
+			if (numero % 5==0) {	
+				System.out.println("El número es divisible por 5");
+			}
+		} else if (numero % 5==0) {	
+			System.out.println("El número es divisible por 5");
+		} else {
+			System.out.println("El número no es ");
+		}
+	}
+
+}
